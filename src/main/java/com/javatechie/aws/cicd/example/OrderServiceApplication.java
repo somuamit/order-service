f467767d3fcdd0e7cde4ec2b2a1147c66e7d6e59
somuamit <somuamit@gmail.com>
2021-01-19 13:38:00 +0530

    Update OrderServiceApplication.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -21,7 +21,7 @@ public class OrderServiceApplication {
 
     @GetMapping
     public List<Order> fetchOrders() {
-        return orderDao.getOrders();
+        return orderDao.getOrders().streams().sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList();
     }
 
     public static void main(String[] args) {
